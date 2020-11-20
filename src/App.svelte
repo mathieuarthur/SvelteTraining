cc54e16fea8ae15f7351c87fbbafe84b0903a323
mathieuarthur <arthur.mathieu@epitech.eu>
2020-11-20 15:06:38 +0100

    mon projet

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Paragraph from './svelte/paragraph.svelte';
+
+	let name = 'world';
+	let src = '/img/bongo.gif';
+	let gifName = 'Bongo';
+	let testText = `Eh oh c'est le truc <strong> de ouf </strong>`;
+	let count = 0;
+
+	function handleClick()
+	{
+		count += 1;
+	}
+	
+</script>
+
+<style>
+	p {
+		color:navajowhite;
+		font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		text-align: center;
+	}
+</style>
+
+<input bind:value={name}>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<p> Est-ce que c'est un P ?</p>
+
+<Paragraph/>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{@html testText}</p>
+
+
+
+<img {src} alt="{gifName}">
---
+++ src/svelte/paragraph.svelte
@@ -0,0 +1,10 @@
+<style>
+    p {
+        color:rebeccapurple;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-size: large;
+        text-align: center;
+    }
+</style>
+
+<p> En fait c'était un p</p>
